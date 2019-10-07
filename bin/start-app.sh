d17b69e648347a6b1a862bf1701b0cc536a36719
Arnaud Banvillet <arnaud@matierenoire.io>
2019-10-07 15:37:49 +0200

    Update start-app.sh

--- bin/start-app.sh
+++ bin/start-app.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+mkdir config/jwt
 echo $JWT_PRIVATE_KEY | base64 -d > config/jwt/private.pem
 echo $JWT_PUBLIC_KEY | base64 -d > config/jwt/public.pem
 # Start default script for PHP apps
