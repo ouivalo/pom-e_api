20335c7692fd5aed5ff5aede9303681388079a37
Arnaud Banvillet <arnaud@matierenoire.io>
2019-10-15 11:23:14 +0200

    Tentative pour ajouter les cles JWT sur Scalingo

--- bin/start-app.sh
+++ bin/start-app.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 echo 'Start copie jwt config file'
-mkdir config/jwtt
-echo $JWT_PRIVATE_KEY | base64 --decode > config/jwtt/private.pem
-echo $JWT_PUBLIC_KEY | base64 --decode > config/jwtt/public.pem
+echo $JWT_PRIVATE_KEY | base64 --decode > /config/jwt/private.pem
+echo $JWT_PUBLIC_KEY | base64 --decode > /config/jwt/public.pem
 # Start default script for PHP apps
 $HOME/bin/run
