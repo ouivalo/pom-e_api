ef4351b39ae407b0e857790337fd3064717bc9c9
Arnaud Banvillet <arnaud@matierenoire.io>
2019-11-19 16:09:38 +0100

    Ajout de script pour démarer CleverCloud

--- clevercloud/post_build.sh
+++ clevercloud/post_build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-
+# Database migrations
 echo 'On joue les migrations'
 ./bin/console doctrine:migrations:migrate
 
+# Copie des clés pour générer les JWT
 echo 'Start copie jwt config file'
 echo $JWT_PRIVATE_KEY_64 | base64 --decode > /app/config/jwt/private.pem
 echo $JWT_PUBLIC_KEY_64 | base64 --decode > /app/config/jwt/public.pem
-# Start default script for PHP apps
-$HOME/bin/run
+
