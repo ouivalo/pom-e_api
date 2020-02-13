9fb88186c95a32ef944d11fce82e491f896c0f19
Arnaud Banvillet <arnaud@matierenoire.io>
2020-02-13 17:05:33 +0100

    Geojson restraint  par status

--- src/Repository/ComposterRepository.php
+++ src/Repository/ComposterRepository.php
@@ -21,6 +21,8 @@ class ComposterRepository extends ServiceEntityRepository
         return $this->createQueryBuilder('c')
             ->andWhere('c.lat IS NOT NULL')
             ->andWhere('c.lng IS NOT NULL')
+            ->andWhere('c.status IN (:map_status)')
+            ->setParameter('map_status', ['Active', 'InProject'])
             ->getQuery()
             ->getResult();
     }
@@ -32,7 +34,9 @@ class ComposterRepository extends ServiceEntityRepository
             ->andWhere('c.lat IS NOT NULL')
             ->andWhere('c.lng IS NOT NULL')
             ->andWhere('c.categorie IN (:quartier_categorie_id)')
+            ->andWhere('c.status = :map_status')
             ->setParameter('quartier_categorie_id', [1, 3])
+            ->setParameter('map_status', 'Active')
             ->getQuery()
             ->getResult();
     }
